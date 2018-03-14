4006d2e95d507a9ca52ea1304633134768382dc7
mireille <mireilleegberts@hotmail.nl>
2018-03-14 11:34:21 +0100

    versie 2.1

--- Frontend-opdracht-2/v2/js/carousel.js
+++ Frontend-opdracht-2/v2/js/carousel.js
@@ -6,23 +6,23 @@
 
 /*eslint 'no-console': 0*/
 var slider = document.querySelector('main');
-var back = document.querySelector('main .back');
+//var back = document.querySelector('main .back');
 var next = document.querySelector('main .next');
 
 
 //actie !
-back.addEventListener("click",function(){
-    slider.classList.add('vorige'); 
-    console.log('test');
-  }
-);
-
 next.addEventListener("click",function(){
-    slider.classList.add('volgende'); 
-    console.log('test');
+    slider.classList.add('volgende');
+    console.log('volgende slide');
   }
 );
 
+//straks aanzetten
+//back.addEventListener("click",function(){
+//    slider.classList.add('vorige'); 
+//  }
+//);
 
-console.log('back');
+
+
 
